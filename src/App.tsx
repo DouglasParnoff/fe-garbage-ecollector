3a7bf23aa5e134a7f375d5738ed6226ce18b71df
douglas parnoff <douglasdasilva.parnoff@adp.com>
2020-06-07 11:37:17 -0300

    add initial Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Home from "./pages/Home";
+
 function App() {
 
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Home />
   );
 }
 
