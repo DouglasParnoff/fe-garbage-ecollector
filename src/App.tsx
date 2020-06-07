e0aedaffc5fe6d679190204e835f95c4c648203c
douglas parnoff <douglasdasilva.parnoff@adp.com>
2020-06-07 11:31:39 -0300

    remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-
-import Header from "./Header";
 
 function App() {
 
-  const [counter, setCounter] = useState(0);
-
-  function handleButtonClick(){
-    setCounter(counter + 1);
-  }
-
   return (
     <div>
-      <Header title="teste2"/>
-      <h1>Conteúdo da app</h1>
-      <h1>{counter}</h1>
-      <h1>{counter * 2}</h1>
-      <h1>{counter * 3}</h1>
-      <button type="button" onClick={handleButtonClick}>Aumentar</button>
+      <h1>Hello World</h1>
     </div>
   );
 }
