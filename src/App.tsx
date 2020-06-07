f669179f377ea9195b3375a669e547ba3e61812c
douglas parnoff <douglasdasilva.parnoff@adp.com>
2020-06-07 11:30:39 -0300

    explore some react concepts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+import Header from "./Header";
+
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
+  function handleButtonClick(){
+    setCounter(counter + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World!</h1>
-      </header>
+    <div>
+      <Header title="teste2"/>
+      <h1>Conteúdo da app</h1>
+      <h1>{counter}</h1>
+      <h1>{counter * 2}</h1>
+      <h1>{counter * 3}</h1>
+      <button type="button" onClick={handleButtonClick}>Aumentar</button>
     </div>
   );
 }
