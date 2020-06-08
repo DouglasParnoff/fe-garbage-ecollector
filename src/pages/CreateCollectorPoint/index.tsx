8b4bbbe71e21822db0d2909755f56fa4959bd7ce
douglas parnoff <douglasdasilva.parnoff@adp.com>
2020-06-07 21:33:04 -0300

    add init of create collector point component

---
+++ src/pages/CreateCollectorPoint/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./styles.css";
+
+const CreateCollectorPoint = () => {
+    return (
+        <div id="">
+
+        </div>
+    );
+}
+
+export default CreateCollectorPoint;
